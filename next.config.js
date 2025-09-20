235025704e17f001d1e224c43ba1c05760e289d7
Sahaya Clement Vincent Martin <60407605+Clementvsc@users.noreply.github.com>
2025-09-20 09:58:18 -0400

    feat: configure static export for GitHub Pagesfeat: configure static export for GitHub PagesUpdate next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/clementvsc.github.io',
+  assetPrefix: '/clementvsc.github.io/',
   trailingSlash: true,
   skipTrailingSlashRedirect: true,
   distDir: 'out',
   images: {
     unoptimized: true
-  },
-  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://clementvsc.github.io' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '' : '',
-  env: {
-    NEXT_PUBLIC_BASE_PATH: process.env.NODE_ENV === 'production' ? '' : ''
   }
 }
 
